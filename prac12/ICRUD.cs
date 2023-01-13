13de0b2e78daca5ae8a0abe3f221dd2e82af3cee
ker1xx <klinkamama@gmail.com>
2023-01-14 00:51:23 +0300

    yup

---
+++ prac12/ICRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac12
+{
+    internal interface ICRUD
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();
+
+    }
+}
--- prac12/MainWindow.xaml.cs
+++ prac12/MainWindow.xaml.cs
@@ -18,13 +18,16 @@ namespace prac12
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, ICRUD
     {
+        DateTime Currentdate_ = DateTime.Now;
+        private List<Notes> Notes_list = y.deserialize<List<Notes>>("Notes.json");
         public MainWindow()
         {
             InitializeComponent();
+
         }
-        private static void currentdate()
+        private  void ListCurrentDate()
         {
             List<Notes> sortedZam = y.Notes_list.Where(x => x.date.Date == Dater.DisplayDate).ToList();
 
@@ -32,8 +35,46 @@ namespace prac12
 
         private void Window_Activated(object sender, EventArgs e)
         {
-            List<Notes> sortedZam = y.Notes_list.Where(x => x.date.Date == Dater.DisplayDate).ToList();
-            ListBox.ItemsSource = sortedZam;
+            Read();
+        }
+        public void Read()
+        {
+            try
+            {
+                List<Notes> sortedZam = y.Notes_list.Where(x => x.date.Date == Dater.DisplayDate).ToList();
+                ListBox.ItemsSource = sortedZam;
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+        public void Update()
+        {
+
+        }
+        public void Create()
+        {
+            string new_note_name = Name_Text.Text;
+            string new_note_description = Description_Text.Text;
+            DateTime new_note_date = Dater.DisplayDate;
+            Notes new_note = new(new_note_name, new_note_description, new_note_date);
+            y.Notes_list.Add(new_note);
+            Read();
+        }
+        public void Delete()
+        {
+
+        }
+
+        private void Create_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
         }
     }
 }
